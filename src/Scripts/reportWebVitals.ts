63546ceddf8f3e641d07bf24ac5153c675678b85
Galyfray <galyfray@gmail.com>
2022-11-22 09:16:17 +0100

    fixed linting issue

--- src/Scripts/reportWebVitals.ts
+++ src/Scripts/reportWebVitals.ts
@@ -1,14 +1,30 @@
-import { ReportHandler } from 'web-vitals';
+import type { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+type handler = (onReport: ReportHandler, reportAllChanges?: boolean | undefined) => void
+
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals').then(
+      (
+        { getCLS, getFID, getFCP, getLCP, getTTFB }:
+          Readonly<{
+            getCLS: handler,
+            getFCP: handler
+            getFID: handler
+            getLCP: handler
+            getTTFB: handler
+          }>
+      ) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      },
+      (reason) => {
+        console.error(reason)
+      }
+    );
   }
 };
 
