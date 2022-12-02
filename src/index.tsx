3e670c74f3e0f9e380da3458e69376e2ecbd5859
Galyfray <galyfray@gmail.com>
2022-12-02 14:44:03 +0100

    fixed typos

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,7 @@ const ROOT = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 ROOT.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
