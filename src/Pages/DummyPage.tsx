2da4e5f25c5890b159f5ff2b26fde4fa3518253d
Galyfray <galyfray@gmail.com>
2022-12-15 15:22:45 +0100

    it is working

--- src/Pages/DummyPage.tsx
+++ src/Pages/DummyPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DummyComponent from "../Components/DummyComponent/DummyComponent";
 import ScopedComponent from "../Components/ScopedComponent/ScopedComponent";
 
-export default function DummyPage(){
+export default function DummyPage():JSX.Element{
 
     return(
         <React.Fragment>
