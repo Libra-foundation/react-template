b6bb82fa91038e6c9658f15e68418f5cb21c6b92
Galyfray <galyfray@gmail.com>
2022-12-15 14:08:35 +0100

    just a small test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App />); // rendering app
   const LINK_ELEMENT = screen.getByText("Test");
   expect(LINK_ELEMENT).toBeInTheDocument();
 });
