e39f9867c2cd5ca9c56a14f175ddcb3e9f53a846
Beladric <kilian.goetz@yahoo.fr>
2022-12-22 15:39:21 +0100

    Change default page and components
    Default page and components was changed to have a custom page like create react app command but for our template.

--- src/Components/DummyComponent/DummyComponent.tsx
+++ src/Components/DummyComponent/DummyComponent.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./DummyStyle.sass";
+import ReactTemplateLogo from "../../Assets/ReactTemplateLogo.svg";
 
 export default function DummyComponent(): JSX.Element {
   return (
     <div className="Div">
-      <p className="test">Test</p>
+      <img src={ReactTemplateLogo} alt="React template logo" className="Logo" />
+      <p className="test">
+        E-constellatio<sup>©</sup> React template
+      </p>
     </div>
   );
 }
--- src/Components/ScopedComponent/ScopedComponent.tsx
+++ src/Components/ScopedComponent/ScopedComponent.tsx
@@ -3,8 +3,11 @@ import cssStyleClassName from "./ScopedStyle.module.sass";
 
 export default function ScopedComponent(): JSX.Element {
   return (
-    <div>
-      <p className={cssStyleClassName.test}>Scoped</p>
+    <div className={cssStyleClassName.Div}>
+      <p className={cssStyleClassName.test}>
+        Welcome In this template for react offer by e-constellatio<sup>©</sup>{" "}
+        company. These lines have scoped CSS written in SASS !
+      </p>
     </div>
   );
 }
