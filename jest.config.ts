8277179b52bd64d3413c93a400f16ff910f1595c
Beladric <kilian.goetz@yahoo.fr>
2023-01-22 16:40:04 +0100

    Useless regex in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -5,12 +5,10 @@ const config: Config = {
     "\\.(css|scss|sass)$": "identity-obj-proxy",
   },
   transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-    "^.+\\.(ts|tsx)?$": "babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
     "^.+\\.svg$": "<rootDir>/svgTransform.js",
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["./src/setupTests.ts"],
 };
