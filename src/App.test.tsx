0aced94c5a7d4a07076cddb45d990abb6b99a81a
Beladric <kilian.goetz@yahoo.fr>
2023-01-22 14:39:40 +0100

    Victory: everything work well

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
 test("renders learn react link", () => {
+  process.env.VITE_TITLE = "React Template development";
   render(<App />);
   const LINK_ELEMENT: HTMLElement = screen.getByText(
     "E-constellatio React Template development"
