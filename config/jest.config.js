a1da66fd1fc2e69e75a142b6f621b8373dd44bbc
Valentin271 <valentin.d77@free.fr>
2023-02-03 18:26:21 +0100

    moved jest config

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,11 +1,12 @@
 /** @returns {Promise<import('jest').Config>} */
 module.exports = async () => ({
+  rootDir: "..",
   moduleNameMapper: {
     "\\.(css|scss|sass)$": "identity-obj-proxy",
   },
   transform: {
     "^.+\\.tsx?$": "babel-jest",
-    "^.+\\.svg$": "<rootDir>/JestConfig/svgTransform.js",
+    "^.+\\.svg$": "<rootDir>/config/jest/svgTransform.js",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testEnvironment: "jsdom",
